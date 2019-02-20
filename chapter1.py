0e6bfa78a3111e6417ca4349d41a917fd2ea61bb
kornellewy <korneliuszlewczuk@gmail.com>
2019-02-20 14:39:23 +0100

    kjn

--- chapter1.py
+++ chapter1.py
@@ -1,3 +1,7 @@
+"""
+Chapter 1 style
+"""
+
 # good if
 if 4 is not 5:
     print("4 is not 5")
@@ -5,9 +9,33 @@ if 4 is not 5:
 if not 4 is 5:
     print("ot 4 is 5")
 # good check
-kjn = []
-if kjn:
-    print(kjn)
+KJN = []
+if KJN:
+    print(KJN)
 
 print("jast use https://www.pylint.org/")
 print("explanation: https://www.youtube.com/watch?v=fFY5103p5-c")
+
+"""
+str to bits
+"""
+def to_str(bytes_or_str):
+    # to sprawdza czy bytes_or_str to bity
+    if isinstance(bytes_or_str, bytes):
+        value = bytes_or_str.decode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
+
+
+def to_bytes(bytes_or_str):
+    # to sprawdza czy bytes_or_str to str
+    if isinstance(bytes_or_str, str):
+        value = bytes_or_str.encode('utf-8')
+    else:
+        value = bytes_or_str
+    return value
+
+"""
+helper functions 
+"""
